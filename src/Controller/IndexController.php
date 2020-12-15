e74cca6367ffc94e7cd6d3e0af8467a73e4064a6
naija moSalah <naija963@gmail.com>
2020-12-15 21:13:04 +0100

    supprission des matiere

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Matiere;
+use App\Form\MatiereType;
 use App\Repository\MatiereRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/index")
+ */
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/index", name="app_index")
+     * @Route("/", name="app_index")
      * @IsGranted("ROLE_USER")
      */
     public function index(MatiereRepository $matiereRepository): Response
@@ -23,6 +29,74 @@ class IndexController extends AbstractController
 
         ]);
     }
+
+    /**
+     * @Route("/new", name="matiere_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $matiere = new Matiere();
+        $form = $this->createForm(MatiereType::class, $matiere);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($matiere);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('matiere_index');
+        }
+
+        return $this->render('matiere/new.html.twig', [
+            'matiere' => $matiere,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="matiere_show", methods={"GET"})
+     */
+    public function show(Matiere $matiere): Response
+    {
+        return $this->render('matiere/show.html.twig', [
+            'matiere' => $matiere,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="matiere_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Matiere $matiere): Response
+    {
+        $form = $this->createForm(MatiereType::class, $matiere);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('matiere_index');
+        }
+
+        return $this->render('matiere/edit.html.twig', [
+            'matiere' => $matiere,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="matiere1_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Matiere $matiere): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$matiere->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($matiere);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_index');
+    }
+
 
 
 }
